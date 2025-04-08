700d44614c50ebb19d0ea6d50d7199ce408b9200
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-04-08 14:51:25 +0200

    inital commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Guided_project___Plan_a_petting_zoo_visit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // using System;
+
+        string[] pettingZoo =
+        {
+        "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+        "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+        "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+        };
+
+        RandomizeAnimals();
+        void RandomizeAnimals()
+        {
+            Random random = new();
+            for (int i = 0; i < pettingZoo.Length; i++)
+            {
+
+                int r = random.Next(i, pettingZoo.Length);
+
+                string temp = pettingZoo[i];
+                pettingZoo[i] = pettingZoo[r];
+                pettingZoo[r] = temp;
+            }
+        }
+
+        // string[,] group = AssignGroup();
+        Console.WriteLine("School A");
+
+        //PrintGroup(group);
+    }
+}
